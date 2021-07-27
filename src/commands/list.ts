dc22c5f376dd74915177f69385470122f6cdd298
Yukai Huang <yukaihuangtw@gmail.com>
2021-07-27 15:43:31 +0800

    Add list command

---
+++ src/commands/list.ts
@@ -0,0 +1,53 @@
+import {Command, flags} from '@oclif/command'
+import cli from 'cli-ux'
+
+import {APIClient} from '../api'
+
+export default class History extends Command {
+  static description = 'List notes'
+
+  static examples = [
+    `$ hackmd-cli list
+
+ID                     Name
+A58r8ehYTlySO94oiC_MUA Note1
+EeNHDGocSTi70ytMMGQaaQ Note2`,
+  ]
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    team: flags.string({
+      char: 't',
+      description: 'team name',
+    }),
+    ...cli.table.flags(),
+  }
+
+  async run() {
+    const {flags} = this.parse(History)
+
+    try {
+      let notes
+      if (flags.team) {
+        notes = await APIClient.listTeamNotes(flags.team)
+      } else {
+        notes = await APIClient.listNotes()
+      }
+
+      cli.table(notes, {
+        id: {
+          header: 'ID',
+        },
+        name: {
+          get: row => (row as any).title
+        }
+      }, {
+        printLine: this.log,
+        ...flags
+      })
+    } catch (e) {
+      this.log('Fetch history failed')
+      this.error(e as string)
+    }
+  }
+}
