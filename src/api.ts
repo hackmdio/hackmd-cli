94e39339197107df0f3a0a4df67fbc85ccb6b315
James Tsai <jamestsai@JamesdeMacBook-Air.local>
2022-02-24 22:57:47 +0800

    feat: update v1-api client config

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 import API from '@hackmd/api'
 
-// import config from './config'
+import config from './config'
 
-export const APIClient = new API("3KY8B7FSEQ7LRRH62E0C0E394K9YNAF8L0JQUY8MUH1I2BDFNF", "http://localhost:3000/api/openAPI/v1")
+export const APIClient = new API(config.accesstoken as string, config.hackmdAPIEndpointURL)
--- src/config.ts
+++ src/config.ts
@@ -4,38 +4,21 @@ import {homedir} from 'os'
 import * as path from 'path'
 
 let configDir
-if (process.env.HMD_CLI_CONFIG_DIR || process.env.CMD_CLI_CONFIG_DIR) {
-  configDir = process.env.HMD_CLI_CONFIG_DIR || process.env.CMD_CLI_CONFIG_DIR || ''
+if (process.env.HMD_CLI_CONFIG_DIR) {
+  configDir = process.env.HMD_CLI_CONFIG_DIR || ''
 } else {
   configDir = path.join(homedir(), '.hackmd')
 }
 
 const configFilePath = path.join(configDir, 'config.json')
-const defaultCookiePath = path.join(homedir(), '.hackmd', 'cookies.json')
 
 const defaultConfig = {
-  cookiePath: defaultCookiePath,
-  serverUrl: 'https://hackmd.io',
-  enterprise: true
-}
-
-function toBooleanConfig(configValue?: string | boolean) {
-  if (configValue && typeof configValue === 'string') {
-    return (configValue === 'true')
-  }
-  return configValue
+  hackmdAPIEndpointURL: 'https://api.hackmd.io/v1'
 }
 
 const envConfig = {
-  cookiePath: process.env.HMD_CLI_COOKIE_PATH || process.env.CMD_CLI_COOKIE_PATH,
-  serverUrl: process.env.HMD_CLI_SERVER_URL || process.env.CMD_CLI_SERVER_URL,
-  enterprise: (process.env.HMD_CLI_COOKIE_PATH || process.env.HMD_CLI_SERVER_URL)
-    ? true
-    : (process.env.CMD_CLI_COOKIE_PATH || process.env.CMD_CLI_SERVER_URL)
-      ? false
-      : toBooleanConfig(process.env.HMD_CLI_ENTERPRISE),
-  loginID: process.env.HMD_CLI_ID || process.env.CMD_CLI_ID,
-  loginPassword: process.env.HMD_CLI_PASSWORD || process.env.CMD_CLI_PASSWORD
+  hackmdAPIEndpointURL: process.env.HMD_API_ENDPOINT_URL,
+  accesstoken: process.env.HMD_API_ACCESS_TOKEN
 }
 
 // look for a readable config file; we can merge it with the env.
@@ -65,55 +48,5 @@ ${err}
 
 // prefer environment config over file config
 const config = defaults(envConfig, readConfig, defaultConfig)
-
-const cookieDirPath = path.dirname(config.cookiePath)
-try {
-  fs.mkdirSync(cookieDirPath)
-} catch (err) {
-  if (err.code !== 'EEXIST') {
-    throw new Error(`
-
-Could not create dir for cookie file at ${cookieDirPath}. Encountered exception:
-
-${err}
-
-`)
-  }
-  // at this point, the directory exists.  if the cookie file does not exist,
-  // ensure the dir is writable (because we will create the file); otherwise
-  // ensure the file itself is writable.
-  let hasExistingCookieFile = false
-  try {
-    fs.existsSync(config.cookiePath)
-    hasExistingConfigFile = true
-  // tslint:disable-next-line: no-unused
-  } catch (ignored) {}
-
-  if (hasExistingCookieFile) {
-    try {
-      fs.accessSync(config.cookiePath, fs.constants.W_OK)
-    } catch (err) {
-      throw new Error(`
-
-Cookie file ${config.cookiePath} is not writable. Encountered exception:
-
-${err}
-
-`)
-    }
-  } else {
-    try {
-      fs.accessSync(cookieDirPath, fs.constants.W_OK)
-    } catch (err) {
-      throw new Error(`
-
-Dir for cookie file at ${cookieDirPath} is not writable. Encountered exception:
-
-${err}
-
-`)
-    }
-  }
-}
 
 export default config
